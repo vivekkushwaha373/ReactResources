bd2ae75f911d862d6ea0383c581d3195c6ea6df7
MyGit <vivekkushwaha734@gmail.com>
2024-06-17 12:31:00 +0530

    React Reasources

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+function App() {
+  //value is the name of the state and setvalue is the function of the state
+  /* curly brackats are used to write pure js  */
+  // onclick takes function
+  
+  return (
+    <div className="App">
+        
+    </div>
+  );
+}
+
+export default App;
